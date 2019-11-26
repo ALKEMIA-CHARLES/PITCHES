6c2b21d52c2326dc858ca6c03894d56afdede042
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-11-26 14:21:59 +0300

    third  deployment to Heroku

--- manage.py
+++ manage.py
@@ -4,12 +4,13 @@ from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Comments
 # import your models here too
 
-app = create_app("development")
+app = create_app("production")
 manager = Manager(app)
 manager.add_command("server", Server)
 
-migrate = Migrate(app,db)
-manager.add_command("db",MigrateCommand)
+migrate = Migrate(app, db)
+manager.add_command("db", MigrateCommand)
+
 
 @manager.command
 def test():
@@ -20,9 +21,12 @@ def test():
     tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, Comments=Comments, User=User) #your models here 
+    # your models here
+    return dict(app=app, db=db, Comments=Comments, User=User)
+
 
 if __name__ == '__main__':
     manager.run()
